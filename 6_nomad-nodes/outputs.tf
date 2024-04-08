dea8c965c063c70f4f79e4b61f2a91272523b564
conradcorbett <conrad.corbett@hashicorp.com>
2024-04-08 11:41:41 -0700

    test

--- 6_nomad-nodes/outputs.tf
+++ 6_nomad-nodes/outputs.tf
@@ -2,6 +2,6 @@ output "nomad_client_x86_asg" {
   value = aws_autoscaling_group.nomad_client_x86_asg.arn
 }
 
-output "nomad_client_arm_asg" {
-  value = aws_autoscaling_group.nomad_client_arm_asg.arn
-}
+#output "nomad_client_arm_asg" {
+#  value = aws_autoscaling_group.nomad_client_arm_asg.arn
+#}
