0530b9615f4492053ce366ff78f43a0d8e5591c5
conradcorbett <conrad.corbett@hashicorp.com>
2024-05-21 16:25:38 -0700

    output

--- 6_nomad-nodes/outputs.tf
+++ 6_nomad-nodes/outputs.tf
@@ -8,4 +8,5 @@ output "nomad_client_x86_asg" {
 
 output "nomad_env_vars" {
   value = "export NOMAD_ADDR=http://${data.terraform_remote_state.nomad_cluster.outputs.nomad_public_endpoint} && export NOMAD_TOKEN=${data.vault_kv_secret_v2.bootstrap.data["SecretID"]}"
+  sensitive = true
 }
