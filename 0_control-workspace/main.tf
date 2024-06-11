2bf7e733a34655b04a71eb4dd86172111b689904
conradcorbett <conrad.corbett@hashicorp.com>
2024-06-11 16:08:54 -0700

    added splunk-tf workspace

--- 0_control-workspace/main.tf
+++ 0_control-workspace/main.tf
@@ -144,6 +144,23 @@ resource "tfe_workspace" "splunk" {
   global_remote_state = true
 }
 
+resource "tfe_workspace" "splunk-tf" {
+  name          = "9_splunk-tf"
+  organization  = var.tfc_organization
+  project_id    = var.tfc_project_id
+
+  vcs_repo {
+    identifier = var.repo_identifier
+    oauth_token_id = var.oauth_token_id
+    branch = var.repo_branch
+  }
+
+  working_directory = "splunk-tf"
+  queue_all_runs = false
+  assessments_enabled = false
+  global_remote_state = true
+}
+
 resource "tfe_workspace_run" "networking" {
   workspace_id    = tfe_workspace.networking.id
 
