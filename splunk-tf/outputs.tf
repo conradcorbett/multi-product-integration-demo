fffd0caad94e65879fd3afba6478823c45d9038a
conradcorbett <conrad.corbett@hashicorp.com>
2024-06-12 13:29:41 -0700

    added vault env vars to copy to cli

--- splunk-tf/outputs.tf
+++ splunk-tf/outputs.tf
@@ -1,8 +1,3 @@
-#output "vault_env_vars" {
-#  value = nonsensitive("export VAULT_ADDR=${data.terraform_remote_state.nomad_cluster.outputs.nomad_public_endpoint} && export NOMAD_TOKEN=${data.vault_kv_secret_v2.bootstrap.data["SecretID"]}")
-#}
-#
-
-output "test" {
-  value = data.aws_instance.nomad_x86_client.public_ip
+output "vault_env_vars" {
+  value = nonsensitive("export VAULT_ADDR=http://${data.aws_instance.nomad_x86_client.public_ip}:8204 && export VAULT_TOKEN=${data.vault_kv_secret_v2.vault-dev-root-token.data["VAULT_ROOT_TOKEN"]}")
 }
